1a15062f896bd6ad674913e6357be46c729580a9
Marko Kotnik <herqapp@gmail.com>
2024-11-30 16:06:21 +0100

    Initial Commit

---
+++ UserManagerApp/UserManagerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserManagerAppApp.swift
+//  UserManagerApp
+//
+//  Created by ANDRAZ TULJAK on 30. 11. 24.
+//
+
+import SwiftUI
+
+@main
+struct UserManagerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
